9a433d4685db1382fe1105a96b1f650ebd03ac46
SOFTEQ\agridiushko <alexey.gridiushko@softeq.com>
2017-11-29 15:47:04 +0300

    no

--- AzureCloudServiceServiceBus/FrontendWebRole/Controllers/HomeController.cs
+++ AzureCloudServiceServiceBus/FrontendWebRole/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FrontendWebRole.Models;
+using Microsoft.ServiceBus.Messaging;
 
 namespace FrontendWebRole.Controllers
 {
@@ -10,21 +12,39 @@ namespace FrontendWebRole.Controllers
     {
         public ActionResult Index()
         {
+            return RedirectToAction("Submit");
+        }
+
+        public ActionResult Submit()
+        {
+            // Get a NamespaceManager which allows you to perform management and
+            // diagnostic operations on your Service Bus queues.
+            var namespaceManager = QueueConnector.CreateNamespaceManager();
+
+            // Get the queue, and obtain the message count.
+            var queue = namespaceManager.GetQueue(QueueConnector.QueueName);
+            ViewBag.MessageCount = queue.MessageCount;
+
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Submit(OnlineOrder order)
         {
-            ViewBag.Message = "Your application description page.";
+            if (ModelState.IsValid)
+            {
+                // Create a message from the order.
+                var message = new BrokeredMessage(order);
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+                // Submit the order.
+                QueueConnector.OrdersQueueClient.Send(message);
+                return RedirectToAction("Submit");
+            }
+            else
+            {
+                return View(order);
+            }
         }
     }
 }
---
+++ AzureCloudServiceServiceBus/FrontendWebRole/QueueConnector.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace FrontendWebRole
+{
+    public static class QueueConnector
+    {
+        // Thread-safe. Recommended that you cache rather than recreating it
+        // on every request.
+        public static QueueClient OrdersQueueClient;
+
+        // Obtain these values from the portal.
+        public const string Namespace = "gredjasb";
+
+        // The name of your queue.
+        public const string QueueName = "queue01";
+
+        public static NamespaceManager CreateNamespaceManager()
+        {
+            // Create the namespace manager which gives you access to
+            // management operations.
+            var uri = ServiceBusEnvironment.CreateServiceUri(
+                "sb", Namespace, String.Empty);
+            var tP = TokenProvider.CreateSharedAccessSignatureTokenProvider(
+                "RootManageSharedAccessKey", "YnOVED/A4+VFVxdGL/qZBV6kLGYP1zprpHKVxXXGSH0=");
+            return new NamespaceManager(uri, tP);
+        }
+
+        public static void Initialize()
+        {
+            // Using Http to be friendly with outbound firewalls.
+            ServiceBusEnvironment.SystemConnectivity.Mode =
+                ConnectivityMode.Http;
+
+            // Create the namespace manager which gives you access to
+            // management operations.
+            var namespaceManager = CreateNamespaceManager();
+
+            // Create the queue if it does not exist already.
+            if (!namespaceManager.QueueExists(QueueName))
+            {
+                namespaceManager.CreateQueue(QueueName);
+            }
+
+            // Get a client to the queue.
+            var messagingFactory = MessagingFactory.Create(
+                namespaceManager.Address,
+                namespaceManager.Settings.TokenProvider);
+            OrdersQueueClient = messagingFactory.CreateQueueClient(
+                "OrdersQueue");
+        }
+    }
+}
